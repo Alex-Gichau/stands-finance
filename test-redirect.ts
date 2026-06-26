ba40ff7783d181e039f2ae98f77295840dbcf411
Alex Gichau <gichaumburu@gmail.com>
2026-06-26 20:02:45 +0300

    local push

--- test-redirect.ts
+++
@@ -1,7 +0,0 @@
-import https from 'https';
-const targetUrl = 'https://wjftrnergydgosatyuzo.supabase.co/auth/v1/authorize?provider=google&redirect_to=https://ais-dev-km4p5ah2577oq3ir42cgsq-261596193432.europe-west2.run.app';
-
-https.get(targetUrl, (res) => {
-  console.log("Status:", res.statusCode);
-  console.log("Location:", res.headers.location);
-});
